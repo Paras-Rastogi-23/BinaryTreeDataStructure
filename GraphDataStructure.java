f2eee08fd21787f194ac3b4180d3cb98bec31b01
Paras <rastogiparas38@gmail.com>
2020-12-14 23:08:22 +0530

    graph BFS commit

--- GraphDataStructure.java
+++ GraphDataStructure.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Stack;
 
 class Vertex {
@@ -17,12 +19,14 @@ class Graph {
     private int adjMat[][];
     private int nVerts;
     private Stack<Integer> s;
+    private Queue<Integer> q;
 
     public Graph() {
         vertexList = new Vertex[MAX_VERTS];
         adjMat = new int[MAX_VERTS][MAX_VERTS];
         nVerts = 0;
         s = new Stack<Integer>();
+        q = new LinkedList<Integer>();
     }
 
     public void addVertex(char lab) {
@@ -38,9 +42,9 @@ class Graph {
         System.out.print(vertexList[v].label + " ");
     }
 
-    public int getAdjUnvisitedVertex (int v) {
-        for(int j = 0; j < nVerts; j++) {
-            if(adjMat[v][j] == 1 && vertexList[j].wasVisited == false) {
+    public int getAdjUnvisitedVertex(int v) {
+        for (int j = 0; j < nVerts; j++) {
+            if (adjMat[v][j] == 1 && vertexList[j].wasVisited == false) {
                 return j;
             }
         }
@@ -52,10 +56,10 @@ class Graph {
         displayVertex(0);
         s.push(0);
 
-        while(!s.isEmpty()) {
+        while (!s.isEmpty()) {
             int v = getAdjUnvisitedVertex(s.peek());
 
-            if(v == -1) {
+            if (v == -1) {
                 s.pop();
             } else {
                 vertexList[v].wasVisited = true;
@@ -64,7 +68,26 @@ class Graph {
             }
         }
     }
+
+    public void bfs() {
+        displayVertex(0);
+        q.add(0);
+        vertexList[0].wasVisited = true;
+        int v2;
+
+        while (q.size() > 0) {
+            int v1 = q.remove();
+
+            while ((v2 = getAdjUnvisitedVertex(v1)) != -1) {
+                displayVertex(v2);
+                vertexList[v2].wasVisited = true;
+                q.add(v2);
+            }
+        }
+    }
 }
+
+
 
 public class GraphDataStructure {
 
@@ -85,8 +108,28 @@ public class GraphDataStructure {
         theGraph.addEdge(4, 5);
         theGraph.addEdge(1, 3);
 
-        System.out.println("Visits: ");
+        System.out.print("DFS Visits: ");
         theGraph.dfs();
+        System.out.println();
+
+        //**************************************************
+        Graph theGraph2 = new Graph();
+        theGraph2.addVertex('A');
+        theGraph2.addVertex('B');
+        theGraph2.addVertex('C');
+        theGraph2.addVertex('D');
+        theGraph2.addVertex('E');
+        theGraph2.addVertex('F');
+
+        theGraph2.addEdge(0, 1);
+        theGraph2.addEdge(1, 2);
+        theGraph2.addEdge(0, 3);
+        theGraph2.addEdge(3, 4);
+        theGraph2.addEdge(4, 5);
+        theGraph2.addEdge(1, 3);
+
+        System.out.print("BFS Visits: ");
+        theGraph2.bfs();
         System.out.println();
     }
 
